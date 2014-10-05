92a9ad7932b71dcb6dc05a116327710941a1729f
Miëtek Bak <mietek@bak.io>
2014-10-05 02:48:20 +0100

    Add missing source line

--- bashmenot.sh
+++ bashmenot.sh
@@ -3,13 +3,14 @@
 declare bashmenot_src_dir
 bashmenot_src_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd -P )
 
-source "${bashmenot_src_dir}/src/log.sh"
-source "${bashmenot_src_dir}/src/expect.sh"
-source "${bashmenot_src_dir}/src/os.sh"
-source "${bashmenot_src_dir}/src/line.sh"
-source "${bashmenot_src_dir}/src/sort.sh"
-source "${bashmenot_src_dir}/src/file.sh"
-source "${bashmenot_src_dir}/src/tar.sh"
-source "${bashmenot_src_dir}/src/date.sh"
 source "${bashmenot_src_dir}/src/curl.sh"
+source "${bashmenot_src_dir}/src/date.sh"
+source "${bashmenot_src_dir}/src/expect.sh"
+source "${bashmenot_src_dir}/src/file.sh"
+source "${bashmenot_src_dir}/src/line.sh"
+source "${bashmenot_src_dir}/src/log.sh"
+source "${bashmenot_src_dir}/src/os.sh"
+source "${bashmenot_src_dir}/src/quote.sh"
 source "${bashmenot_src_dir}/src/s3.sh"
+source "${bashmenot_src_dir}/src/sort.sh"
+source "${bashmenot_src_dir}/src/tar.sh"
