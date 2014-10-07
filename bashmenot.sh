cecb2ad5f973ab49e4edeb38cf93c5ffd6f398e9
Miëtek Bak <mietek@bak.io>
2014-10-07 07:11:53 +0100

    Update packaging

--- bashmenot.sh
+++ bashmenot.sh
@@ -1,15 +1,14 @@
-declare bashmenot_src_dir
-bashmenot_src_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd -P )
+declare BASHMENOT_TOP_DIR
+BASHMENOT_TOP_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd -P )
 
-
-source "${bashmenot_src_dir}/src/log.sh"
-source "${bashmenot_src_dir}/src/expect.sh"
-source "${bashmenot_src_dir}/src/os.sh"
-source "${bashmenot_src_dir}/src/quote.sh"
-source "${bashmenot_src_dir}/src/line.sh"
-source "${bashmenot_src_dir}/src/sort.sh"
-source "${bashmenot_src_dir}/src/date.sh"
-source "${bashmenot_src_dir}/src/file.sh"
-source "${bashmenot_src_dir}/src/tar.sh"
-source "${bashmenot_src_dir}/src/curl.sh"
-source "${bashmenot_src_dir}/src/s3.sh"
+source "${BASHMENOT_TOP_DIR}/src/log.sh"
+source "${BASHMENOT_TOP_DIR}/src/expect.sh"
+source "${BASHMENOT_TOP_DIR}/src/os.sh"
+source "${BASHMENOT_TOP_DIR}/src/quote.sh"
+source "${BASHMENOT_TOP_DIR}/src/line.sh"
+source "${BASHMENOT_TOP_DIR}/src/sort.sh"
+source "${BASHMENOT_TOP_DIR}/src/date.sh"
+source "${BASHMENOT_TOP_DIR}/src/file.sh"
+source "${BASHMENOT_TOP_DIR}/src/tar.sh"
+source "${BASHMENOT_TOP_DIR}/src/curl.sh"
+source "${BASHMENOT_TOP_DIR}/src/s3.sh"
