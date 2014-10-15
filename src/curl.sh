b8136977e8c82429e4f79be14d29dc4a2465159e
Miëtek Bak <mietek@bak.io>
2014-10-15 20:09:57 +0100

    Add user-friendly HTTP code descriptions

--- src/curl.sh
+++ src/curl.sh
@@ -1,11 +1,63 @@
+function echo_http_code_description () {
+	local code
+	expect_args code -- "$@"
+
+	case "${code}" in
+	'200')	echo 'done';;
+	'201')	echo '201 Created';;
+	'202')	echo '202 Accepted';;
+	'203')	echo '203 Non-authoritative information';;
+	'204')	echo '204 No content';;
+	'205')	echo '205 Reset content';;
+	'206')	echo '206 Partial content';;
+	'400')	echo '400 Bad request';;
+	'401')	echo '401 Unauthorized';;
+	'402')	echo '402 Payment required';;
+	'403')	echo '403 Forbidden';;
+	'404')	echo '404 Not found';;
+	'405')	echo '405 Method not allowed';;
+	'406')	echo '406 Not acceptable';;
+	'407')	echo '407 Proxy authentication required';;
+	'408')	echo '408 Request timeout';;
+	'409')	echo '409 Conflict';;
+	'410')	echo '410 Gone';;
+	'411')	echo '411 Length required';;
+	'412')	echo '412 Precondition failed';;
+	'413')	echo '413 Request entity too large';;
+	'414')	echo '414 Request URI too long';;
+	'415')	echo '415 Unsupported media type';;
+	'416')	echo '416 Requested range';;
+	'417')	echo '417 Expectation failed';;
+	'418')	echo "418 I'm a teapot";;
+	'419')	echo '419 Authentication timeout';;
+	'420')	echo '420 Enhance your calm';;
+	'426')	echo '426 Upgrade required';;
+	'428')	echo '428 Precondition required';;
+	'429')	echo '429 Too many requests';;
+	'431')	echo '431 Request header fields too large';;
+	'451')	echo '451 Unavailable for legal reasons';;
+	'500')	echo '500 Internal server error';;
+	'501')	echo '501 Not implemented';;
+	'502')	echo '502 Bad gateway';;
+	'503')	echo '503 Service unavailable';;
+	'504')	echo '504 Gateway timeout';;
+	'505')	echo '505 HTTP version not supported';;
+	'506')	echo '506 Variant also negotiates';;
+	'510')	echo '510 Not extended';;
+	'511')	echo '511 Network authentication required';;
+	*)	echo "${code} (unknown)"
+	esac
+}
+
+
 function curl_do () {
 	local url
 	expect_args url -- "$@"
 	shift
 
-	local status response
+	local status code
 	status=0
-	if ! response=$(
+	if ! code=$(
 		curl "${url}"                      \
 			--fail                     \
 			--location                 \
@@ -18,14 +70,9 @@ function curl_do () {
 		status=1
 	fi
 
-	case "${response}" in
-	'200')
-		log_end 'done';;
-	'2'*)
-		log_end "done, ${response}";;
-	*)
-		log_end "${response}"
-	esac
+	local code_description
+	code_description=$( echo_http_code_description "${code}" ) || die
+	log_end "${code_description}"
 
 	return "${status}"
 }
