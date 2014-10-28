6e8577d1066cbc26d7644a19d9239dce620a3cc2
Miëtek Bak <mietek@bak.io>
2014-10-28 21:35:01 +0000

    Simplify date formatting functions

--- src/date.sh
+++ src/date.sh
@@ -1,37 +1,24 @@
 case "$( detect_os )" in
 'linux-'*)
-	function format_date () {
-		date "$@" || die
+	function format_http_date () {
+		date --utc --rfc-2822 "$@" || die
 	}
 	;;
 *)
-	function format_date () {
-		gdate "$@" || die
+	function format_http_date () {
+		gdate --utc --rfc-2822 "$@" || die
 	}
 esac
 
 
-function format_http_date () {
-	format_date --utc --rfc-2822 "$@" || die
-}
-
-
-function format_timestamp () {
-	format_date --utc +'%Y%m%d%H%M%S' "$@" || die
-}
-
-
-function get_timestamp_date () {
-	local timestamp
-	expect_args timestamp -- "$@"
-
-	echo "${timestamp:0:4}-${timestamp:4:2}-${timestamp:6:2}"
-}
-
-
-function get_timestamp_time () {
-	local timestamp
-	expect_args timestamp -- "$@"
-
-	echo "${timestamp:8:2}:${timestamp:10:2}:${timestamp:12:2}"
-}
+case "$( detect_os )" in
+'linux-'*)
+	function format_date () {
+		date --utc +'%Y-%m-%d' "$@" || die
+	}
+	;;
+*)
+	function format_date () {
+		gdate --utc +'%Y-%m-%d' "$@" || die
+	}
+esac
