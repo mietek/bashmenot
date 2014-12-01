78303a4385e75b64cb2a4256fb533e4efd188a5a
Miëtek Bak <mietek@bak.io>
2014-12-01 12:17:44 +0000

    Improve naming

--- src.sh
+++ src.sh
@@ -1,20 +1,20 @@
 set -o pipefail
 
-export BASHMENOT_INSTALL
-BASHMENOT_INSTALL=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd -P )
+export BASHMENOT_DIR
+BASHMENOT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd -P )
 
-source "${BASHMENOT_INSTALL}/src/date.sh"
-source "${BASHMENOT_INSTALL}/src/sort.sh"
-source "${BASHMENOT_INSTALL}/src/log.sh"
-source "${BASHMENOT_INSTALL}/src/expect.sh"
-source "${BASHMENOT_INSTALL}/src/platform.sh"
-source "${BASHMENOT_INSTALL}/src/line.sh"
-source "${BASHMENOT_INSTALL}/src/file.sh"
-source "${BASHMENOT_INSTALL}/src/hash.sh"
-source "${BASHMENOT_INSTALL}/src/tar.sh"
-source "${BASHMENOT_INSTALL}/src/git.sh"
-source "${BASHMENOT_INSTALL}/src/curl.sh"
-source "${BASHMENOT_INSTALL}/src/s3.sh"
+source "${BASHMENOT_DIR}/src/date.sh"
+source "${BASHMENOT_DIR}/src/sort.sh"
+source "${BASHMENOT_DIR}/src/log.sh"
+source "${BASHMENOT_DIR}/src/expect.sh"
+source "${BASHMENOT_DIR}/src/platform.sh"
+source "${BASHMENOT_DIR}/src/line.sh"
+source "${BASHMENOT_DIR}/src/file.sh"
+source "${BASHMENOT_DIR}/src/hash.sh"
+source "${BASHMENOT_DIR}/src/tar.sh"
+source "${BASHMENOT_DIR}/src/git.sh"
+source "${BASHMENOT_DIR}/src/curl.sh"
+source "${BASHMENOT_DIR}/src/s3.sh"
 
 
 bashmenot_self_update () {
@@ -22,7 +22,7 @@ bashmenot_self_update () {
 		return 0
 	fi
 
-	if [[ ! -d "${BASHMENOT_INSTALL}/.git" ]]; then
+	if [[ ! -d "${BASHMENOT_DIR}/.git" ]]; then
 		return 1
 	fi
 
@@ -32,14 +32,14 @@ bashmenot_self_update () {
 	log_begin 'Self-updating bashmenot...'
 
 	local commit_hash
-	if ! commit_hash=$( git_update_into "${url}" "${BASHMENOT_INSTALL}" ); then
+	if ! commit_hash=$( git_update_into "${url}" "${BASHMENOT_DIR}" ); then
 		log_end 'error'
 		return 1
 	fi
 	log_end "done, ${commit_hash:0:7}"
 
 	BASHMENOT_NO_SELF_UPDATE=1 \
-		source "${BASHMENOT_INSTALL}/src.sh"
+		source "${BASHMENOT_DIR}/src.sh"
 }
 
 
