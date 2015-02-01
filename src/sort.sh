f594721e6aaf3597ce4a53e2f5ab80b0d3cc19b6
Miëtek Bak <mietek@bak.io>
2015-02-01 22:35:31 +0000

    Add sort_do

--- src/sort.sh
+++ src/sort.sh
@@ -1,16 +1,21 @@
 case $( uname -s ) in
 'Linux'|'FreeBSD')
-	sort_natural () {
-		sort -V "$@" || return 0
+	sort_do () {
+		sort "$@" || return 0
 	}
 	;;
 *)
-	sort_natural () {
-		gsort -V "$@" || return 0
+	sort_do () {
+		gsort "$@" || return 0
 	}
 esac
 
 
+sort_natural () {
+	sort_do -V "$@" || return 0
+}
+
+
 sort0_natural () {
-	sort_natural -z "$@" || return 0
+	sort_do -Vz "$@" || return 0
 }
